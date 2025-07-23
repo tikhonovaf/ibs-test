1e8b17c97a8557e820890b7f289b995a98236938
atikhonov <tikhonovaf@gmail.com>
2025-07-24 00:26:14 +0300

    Коммит в ветку resilience4j

--- src/main/java/ru/ibs/planeta/feign/ExchangeClient.java
+++ src/main/java/ru/ibs/planeta/feign/ExchangeClient.java
@@ -1,5 +1,7 @@
 package ru.ibs.planeta.feign;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.ibs.planeta.model.dto.DepartmentsResponseDto;
@@ -8,9 +10,13 @@ import ru.ibs.planeta.model.dto.ProjectsResponseDto;
 @FeignClient(name = "external-service", url = "${loader.departments-url}")
 public interface ExchangeClient {
 
+    @CircuitBreaker(name = "external-service")
+    @Retry(name = "external-service")
     @GetMapping("/departments")
     DepartmentsResponseDto getDepartments();
 
+    @CircuitBreaker(name = "external-service")
+    @Retry(name = "external-service")
     @GetMapping("/projects")
     ProjectsResponseDto getProjects();
 
